fcd4cb2b46a7d8491148bb7c44bf25008efbbde9
Beroxqyc <y1057899916u@gmail.com>
2025-02-12 15:39:12 +0100

    p2

---
+++ js/P1_Escena.js
@@ -0,0 +1,111 @@
+/**
+ * Escena.js
+ * 
+ * Practica AGM #1. Escena basica en three.js
+ * Seis objetos organizados en un grafo de escena con
+ * transformaciones, animacion basica y modelos importados
+ * 
+ * @author 
+ * 
+ */
+
+// Modulos necesarios
+/*******************
+ * TO DO: Cargar los modulos necesarios
+ *******************/
+import * as THREE from "../lib/three.module.js";
+import { GLTFLoader} from "../lib/GLTFLoader.module.js";
+
+// Variables de consenso
+let renderer, scene, camera;
+
+// Otras globales
+/*******************
+ * TO DO: Variables globales de la aplicacion
+ *******************/
+let esferaCubo;
+let angulo = 0;
+
+// Acciones
+init();
+loadScene();
+render();
+
+function init()
+{
+    // Motor de render
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    /*******************
+    * TO DO: Completar el motor de render y el canvas
+    *******************/
+    document.getElementById('container').appendChild( renderer.domElement );
+
+    // Escena
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0.5,0.5,0.5);
+
+    // Camara
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1,1000);
+    camera.position.set( 0.5, 2, 7 );
+    camera.lookAt( new THREE.Vector3(0,1,0) );
+}
+
+function loadScene()
+{
+    const material = new THREE.MeshNormalMaterial( );
+    
+    const geoPenrtagon = new THREE.PolyhedronGeometry();
+
+    /*******************
+    * TO DO: Construir un suelo en el plano XZ
+    *******************/
+    const suelo = new THREE.Mesh( new THREE.PlaneGeometry(10,10, 10,10), material );
+    suelo.rotation.x = -Math.PI / 2;
+    scene.add(suelo);
+
+    /*******************
+    * TO DO: Construir una escena con 5 figuras diferentes posicionadas
+    * en los cinco vertices de un pentagono regular alredor del origen
+    *******************/
+    const loader = new THREE.ObjectLoader();
+    loader.load( 'models/soldado/soldado.json', 
+        function(objeto){
+            objeto.rotation.y = Math.PI/2;
+            scene.add(objeto);
+            objeto.position.y = 0;
+        }
+    );
+
+    const glloader = new GLTFLoader();
+
+    glloader.load( 'models/robota/scene.gltf', function ( gltf ) {
+        gltf.scene.position.y = 1;
+        scene.add( gltf.scene );
+    });
+
+    /*******************
+    * TO DO: Añadir a la escena un modelo importado en el centro del pentagono
+    *******************/
+
+    /*******************
+    * TO DO: Añadir a la escena unos ejes
+    *******************/
+
+    scene.add(new THREE.AxesHelper(3));
+}
+
+function update()
+{
+    /*******************
+    * TO DO: Modificar el angulo de giro de cada objeto sobre si mismo
+    * y del conjunto pentagonal sobre el objeto importado
+    *******************/
+}
+
+function render()
+{
+    requestAnimationFrame( render );
+    update();
+    renderer.render( scene, camera );
+}

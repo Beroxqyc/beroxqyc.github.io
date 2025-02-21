0995a277dd59b3cc8edd8020d446f18dcbbe5b28
Beroxqyc <y1057899916u@gmail.com>
2025-02-21 17:06:26 +0100

    nn

--- js/P2_EscenaAnimada.js
+++ js/P2_EscenaAnimada.js
@@ -14,10 +14,10 @@
  * TO DO: Cargar los modulos necesarios
  *******************/
 import * as THREE from "../lib/three.module.js";
-import { GLTFLoader } from "../lib/GLTFLoader.module.js";
-import { OrbitControls } from "../lib/OrbitControls.module.js";
-import { TWEEN } from "../lib/tween.module.min.js";
-import { GUI } from "../lib/lil-gui.module.min.js";
+import {GLTFLoader} from "../lib/GLTFLoader.module.js";
+import {OrbitControls} from "../lib/OrbitControls.module.js";
+import {TWEEN} from "../lib/tween.module.min.js";
+import {GUI} from "../lib/lil-gui.module.min.js";
 
 // Variables de consenso
 let renderer, scene, camera;
@@ -26,9 +26,9 @@ let renderer, scene, camera;
 /*******************
  * TO DO: Variables globales de la aplicacion
  *******************/
-let cameraControls, effectController;
-let esferaCubo, cubo, esfera;
+let pentagon;
 let angulo = 0;
+let cameraControls, effectController;
 
 // Acciones
 init();
@@ -41,20 +41,18 @@ function init()
     // Motor de render
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
-
     /*******************
     * TO DO: Completar el motor de render y el canvas
     *******************/
-    document.getElementById('container').appendChild( renderer.domElement );
+   document.getElementById('container').appendChild( renderer.domElement );
 
     // Escena
     scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0.5, 0.5, 0.5 );
-
-    // Camara
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1,1000);
-    camera.position.set( 0.5, 2, 7 );
+    scene.background = new THREE.Color(0.5,0.5,0.5);
     
+    // Camara
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 1,1000);
+    camera.position.set( 0.5, 2, 7 );
     /*******************
     * TO DO: Añadir manejador de camara (OrbitControls)
     *******************/
@@ -62,17 +60,79 @@ function init()
     cameraControls.target.set(0,1,0);
     camera.lookAt( new THREE.Vector3(0,1,0) );
 
-    renderer.domElement.addEventListener('dblclick', animate );
 }
 
 function loadScene()
 {
-    const material = new THREE.MeshNormalMaterial( {wireframe:false} );
-
     /*******************
     * TO DO: Misma escena que en la practica anterior
     *******************/
-   
+    const material = new THREE.MeshNormalMaterial( );
+    const sueloMaterial = new THREE.MeshBasicMaterial( { color: 'cyan', wireframe: true } );
+
+    /*******************
+    * TO DO: Construir un suelo en el plano XZ
+    *******************/
+    const suelo = new THREE.Mesh( new THREE.PlaneGeometry(10,10, 10,10), sueloMaterial );
+    suelo.rotation.x = -Math.PI / 2;
+    scene.add(suelo);
+
+    /*******************
+    * TO DO: Construir una escena con 5 figuras diferentes posicionadas
+    * en los cinco vertices de un pentagono regular alredor del origen
+    *******************/
+    const figuras = [
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.SphereGeometry(0.5,20,20),
+    new THREE.ConeGeometry(0.5,1,20),
+    new THREE.TorusGeometry(0.5,0.2,16,100),
+    new THREE.DodecahedronGeometry(0.5)
+    ];
+
+    pentagon = new THREE.Object3D();
+
+    for (let i = 0; i < 5; i++) {
+        const angle = 2 * Math.PI * i / 5;
+        const figura = new THREE.Mesh( figuras[i], material );
+        figura.position.set( Math.cos(angle) * 2, 0.7, Math.sin(angle) * 2 );
+        figuras.push(figura);
+        pentagon.add(figura);
+    }
+
+    scene.add(pentagon);
+
+    /*******************
+    * TO DO: Añadir a la escena un modelo importado en el centro del pentagono
+    *******************/
+
+    const loader = new THREE.ObjectLoader();
+    loader.load( 'models/soldado/soldado.json', 
+        function(objeto){
+            scene.add(objeto);
+            objeto.position.y = 0;
+        },
+    );
+
+    const glloader = new GLTFLoader();
+
+    glloader.load( 'models/robota/scene.gltf', function ( gltf ) {
+        gltf.scene.position.x = 0.5;
+        gltf.scene.rotation.y = -Math.PI/2;
+        scene.add( gltf.scene );
+        console.log("ROBOT");
+        console.log(gltf);
+    
+    }, undefined, function ( error ) {
+    
+        console.error( error );
+    
+    } );
+
+    /*******************
+    * TO DO: Añadir a la escena unos ejes
+    *******************/
+    scene.add( new THREE.AxesHelper(3) );
+    pentagon.add( new THREE.AxesHelper(3) );
 
 }
 
